2a38071b6f4d944a8ac75058bcc40c477c377df8
Victor Pires <vctprs@gmail.com>
2021-04-23 00:53:29 -0300

    Abertura de pasta para arquivos convertidos

--- Conversor de imagem.py
+++ Conversor de imagem.py
@@ -1,21 +1,27 @@
 import os
 from PIL import Image
 
-def Conversor(path):
-    files = []
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
-        for file in f:
-            if '.jfif' in file:
-                files.append(os.path.join(r, file))         
-    for f in files:
-        im = Image.open(f)
-        rgb_im = im.convert("RGB")
-        f = f.replace('.jfif','')
-        rgb_im.save(f+".jpg")
-        print(f'O arquivo {f} foi convertido para JPG!')
-
-
 path = os.getcwd()
-Conversor(path)
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.jfif' in file:
+            files.append(os.path.join(r, file))         
+
+directory = "Convertidos em JPG"  
+parent_dir = os.getcwd()
+path_pasta = os.path.join(parent_dir, directory)
+os.mkdir(path_pasta)
+
+for f in files:
+    im = Image.open(f)
+    rgb_im = im.convert("RGB")
+    f = f.replace('.jfif','')
+    splitpath = os.path.split(f)
+    splitpath = list(splitpath)
+    finalpath = os.path.join(splitpath[0],path_pasta,splitpath[1])
+    rgb_im.save(finalpath+".jpg")
+    print(f'O arquivo {f} foi convertido para JPG e está na pasta Convertidos em JPG!')
+
 input()
